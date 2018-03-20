4b10f86fdcd11b510ab9010b8c042cfc41adcec9
Ethan Harris <ethanharris5711@gmail.com>
2018-03-20 08:49:13 -0400

    CollectionAlbumViewComplete

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,17 +1,29 @@
+$( document ).ready(function() {
+
 var animatePoints = function() {
 
-     var points = document.getElementsByClassName('point');
+var revealPoint = function() {
 
-     var revealPoint = function(index) {
-         points[index].style.opacity = 1;
-         points[index].style.transform = "scaleX(1) translateY(0)";
-         points[index].style.msTransform = "scaleX(1) translateY(0)";
-         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
-     }
+        $(this).css({
+             opacity: 1,
+             transform: 'scaleX(1) translateY(0)'
+         });
+          $.each($('.point'), revealPoint);
+    };
 
-     for (var i=0; i< points.length; i++) {
-       revealPoint(i);
-     }
 
-  
- };
+ $(window).load (function() {
+
+   if ($(window).height() > 950) {
+        animatePoints();
+   }
+
+
+    var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+
+    $(window).scroll(function(event) {
+      if ($(window).scrollTop() >= scrollDistance) {
+              animatePoints();}
+         }
+       };
+});
